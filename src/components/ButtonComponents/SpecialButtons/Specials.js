30d679bed32f0ebb96ee5f33c9b28e5efc02aa55
Nicole white <you@example.com>
2019-12-10 12:53:35 -1000

    step 2

--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -1,10 +1,12 @@
 import React from "react";
+import {specials} from "../../../data";
 
 //import any components needed
 
 //Import your array data to from the provided data file
 
 const Specials = () => {
+  const[special, setSpecial] = useState(specials);
   // STEP 2 - add the imported data to state
 
   return (
